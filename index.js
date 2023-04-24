1f4e4215d83a303830685af3a6dd2f38f843fc62
arnob999 <arnob196@gmail.com>
2023-04-24 23:56:41 +0600

    mongo

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //require('crypto').randomBytes(64).toString("hex")
 
-// require('dotenv').config()
+//environment variable
+require('dotenv').config()
+
+
 //2 middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgfn0ly.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 async function run() {
 
