32431a87f48a097bfc000ab63944a8e560d118e3
arnob999 <arnob196@gmail.com>
2023-04-30 01:32:14 +0600

    seller authorization

--- index.js
+++ index.js
@@ -167,6 +167,33 @@ async function run() {
             res.send(result);
         })
 
+
+
+
+
+
+        //validate user as Admin with hook "useAdmin"
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+        //validate user as Seller with hook "useSeller"
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
+        //validate user as Buyer with hook "useBuyer"
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        })
+
     }
 
 
